3853876b708d09233756a768c0e4352147bc3dd8
sebastianpinzon19 <sebastian.pinzonr@soy.sena.edu.co>
2026-05-21 22:34:33 -0500

    fix(head): prefer PNG favicon and fallback /favicon.ico

--- app/head.tsx
+++ app/head.tsx
@@ -1,9 +1,9 @@
 export default function Head() {
   return (
     <>
-      <link rel="icon" href="/sunair-logo.png" />
-      <link rel="shortcut icon" href="/sunair-logo.png" />
-      <link rel="apple-touch-icon" href="/sunair-logo.png" />
+      <link rel="icon" type="image/png" href="/sunair-logo.png" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/sunair-logo.png" />
       <meta name="theme-color" content="#003366" />
     </>
   );
