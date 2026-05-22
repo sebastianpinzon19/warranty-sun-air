8247d8bf313e400140c0bbdf5188ff28a673c973
sebastianpinzon19 <sebastian.pinzonr@soy.sena.edu.co>
2026-05-21 23:18:57 -0500

    chore(favicon): remove app route causing prerender error

--- app/favicon.ico/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse, NextRequest } from 'next/server';
-
-export async function GET(request: NextRequest) {
-  // Use an absolute URL based on the incoming request to avoid prerender errors
-  const target = new URL('/favicon_io/favicon.ico', request.url);
-  return NextResponse.redirect(target.toString(), 302);
-}
