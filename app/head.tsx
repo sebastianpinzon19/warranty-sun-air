9744f77c80934a01f2e0fb4000ccc267e1e061f2
sebastianpinzon19 <sebastian.pinzonr@soy.sena.edu.co>
2026-05-21 22:32:37 -0500

    feat(head): set favicon to sunair logo via app/head.tsx

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="icon" href="/sunair-logo.png" />
+      <link rel="shortcut icon" href="/sunair-logo.png" />
+      <link rel="apple-touch-icon" href="/sunair-logo.png" />
+      <meta name="theme-color" content="#003366" />
+    </>
+  );
+}
