ec076c0f039f6104cdd2dfecb334e9c1264cebfb
sebastianpinzon19 <sebastian.pinzonr@soy.sena.edu.co>
2026-05-21 23:10:41 -0500

    chore(head): use prepared favicon assets from /favicon_io

--- app/head.tsx
+++ app/head.tsx
@@ -1,9 +1,11 @@
 export default function Head() {
   return (
     <>
-      <link rel="icon" type="image/png" href="/sunair-logo.png" />
-      <link rel="shortcut icon" href="/favicon.ico" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/sunair-logo.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon_io/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon_io/favicon-16x16.png" />
+      <link rel="shortcut icon" href="/favicon_io/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/favicon_io/apple-touch-icon.png" />
+      <link rel="manifest" href="/favicon_io/site.webmanifest" />
       <meta name="theme-color" content="#003366" />
     </>
   );
